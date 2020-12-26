ab02edcb5086950be5cd796984658d6c8533a2af
Sonnicon <sonniconn@gmail.com>
2020-12-26 02:33:55 +0000

    Groovy rewrite

--- src/sonnicon/groovymodmod/GroovyModMod.groovy
+++ src/sonnicon/groovymodmod/GroovyModMod.groovy
@@ -3,32 +3,12 @@ package sonnicon.groovymodmod
 import mindustry.Vars
 import mindustry.mod.Mod
 
-class GroovyModMod extends Mod{
+class GroovyModMod extends Mod {
     @Override
-    void init(){
-        GroovyClassLoader gcl = new GroovyClassLoader(getClass().getClassLoader());
-        Vars.modDirectory.findAll().each(fi -> {
-            if(fi.extension().equals("jar") || fi.extension().equals("groovy")){
-                try{
-                    gcl.addURL(fi.file().toURI().toURL());
-                }catch(MalformedURLException e){
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        try{
-            GroovyScriptEngine gse = new GroovyScriptEngine(Vars.modDirectory.path(), gcl);
-            Vars.modDirectory.findAll().each(fi -> {
-                if(!fi.extension().equals("groovy")) return;
-                try{
-                    gse.run(fi.name(), "");
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-            });
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+    void init() {
+        GroovyClassLoader gcl = new GroovyClassLoader(getClass().getClassLoader())
+        Vars.modDirectory.findAll().each { if (it.extension() == "jar" || it.extension() == "groovy") gcl.addURL(it.file().toURI().toURL()) }
+        GroovyScriptEngine gse = new GroovyScriptEngine(Vars.modDirectory.path(), gcl)
+        Vars.modDirectory.findAll().each { if (it.extension() == "groovy") gse.run(it.name(), "") }
     }
 }
